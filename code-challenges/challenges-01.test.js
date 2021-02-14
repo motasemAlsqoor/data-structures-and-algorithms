c91ae95006ec4149a40146180f9588acb062ebb5
motasemAlsqoor <motasem900khaled@gmail.com>
2021-02-14 16:28:59 +0200

    forth

--- code-challenges/challenges-01.test.js
+++ code-challenges/challenges-01.test.js
@@ -93,10 +93,14 @@ Return the modified array.
 
 const addValues = (arr, value) => {
   // Solution code here...
+  arr.push(value);
 };
 
 const addNumbers = (num, arr, times, callback) => {
   // Solution code here...
+  for(let i=0;i<times;i++){
+    addValues(arr,num);
+  }
 };
 
 /* ------------------------------------------------------------------------------------------------
